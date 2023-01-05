1a10cc2456b6f1ccc0a62c9d3a5cfdd71487c811
Ferhat <fkmll2121@gmail.com>
2023-01-05 23:20:12 +0300

    configure index.js file
    Now the server can stand up

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+const PORT = 4000 || process.env.PORT;
+
+app.get("/", (req, res) => {
+    res.send("Hello World");
+});
+
+app.listen(PORT, () => {
+    console.log(`App started on http://localhost:${PORT}`);
+});
