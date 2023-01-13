6e199cd5e466c21463b473dd136e8afab20af2c0
Ferhat <fkmll2121@gmail.com>
2023-01-13 13:20:36 +0300

    Write a function for login operations
    A login method was written where login information is received and checked.
    
    The validateUserInput function was written to check whether the form information is comming or not.
    
    The comparePassword method was written to check the password.

--- controller/authController.js
+++ controller/authController.js
@@ -1,5 +1,9 @@
+const asyncErrorWrapper = require("express-async-handler");
+
 const User = require("../models/user");
 const { sendJwtToClient } = require("../helpers/authorization/tokenHelpers");
+const { validateUserInput } = require("../helpers/input/inputHelpers");
+const { comparePassword } = require("../helpers/input/inputHelpers");
 
 const postRegister = async (req, res, next) => {
     try {
@@ -35,7 +39,46 @@ const getRegister = async (req, res, next) => {
     res.render("register");
 };
 
+const login = asyncErrorWrapper(async (req, res, next) => {
+    const { email, password } = req.body;
+    console.log(req.body);
+
+    if (!validateUserInput(email, password)) {
+        return next(new CustomError("Please check your inputs", 400));
+    }
+
+    const user = await User.findOne({ email }).select("+password");
+
+    console.log(user);
+
+    //check mail
+    if (!user) {
+        res.render("login", {
+            mailError: "Please check your e-mail address.",
+        });
+    }
+
+    //check password
+    if (!comparePassword(password, user.password)) {
+        res.render("login", {
+            passwordError: "Please check your password.",
+        });
+    }
+
+    sendJwtToClient(user, res);
+
+    res.status(200).json({
+        success: true,
+    });
+});
+
+const renderLoginPage = async (req, res, next) => {
+    res.render("login");
+};
+
 module.exports = {
     postRegister,
     getRegister,
+    login,
+    renderLoginPage,
 };
--- router/auth.js
+++ router/auth.js
@@ -1,8 +1,15 @@
 const router = require("express").Router();
 
-const { postRegister, getRegister } = require("../controller/authController");
+const {
+    postRegister,
+    getRegister,
+    login,
+    renderLoginPage,
+} = require("../controller/authController");
 
 router.post("/register", postRegister);
+router.post("/login", login);
+router.get("/login", renderLoginPage);
 router.get("/register", getRegister);
 
 module.exports = router;
