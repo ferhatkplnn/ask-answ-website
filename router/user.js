93bcd064ead87e959ab37fb062fcbe972df871e7
Ferhat <fkmll2121@gmail.com>
2023-01-18 11:36:23 +0300

    Create a method to get all user information
    Created getAllUsers method that freezes all users' informations.

--- router/user.js
+++ router/user.js
@@ -1,10 +1,11 @@
 const express = require("express");
-const { getSingleUser } = require("../controller/userController");
+const { getSingleUser, getAllUsers } = require("../controller/userController");
 const {
     checkUserExist,
 } = require("../middlewares/database/databaseErrorHelpers");
 const router = express.Router();
 
+router.get("/", getAllUsers);
 router.get("/:id", checkUserExist, getSingleUser);
 
 module.exports = router;
