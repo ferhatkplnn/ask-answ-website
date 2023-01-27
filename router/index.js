a44a1cee318b11596e598080965173ee2fe4495a
Ferhat <fkmll2121@gmail.com>
2023-01-27 19:23:17 +0300

    Create the admin router
    An admin router was created to access admin process.

--- router/index.js
+++ router/index.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const auth = require("./auth");
 const questions = require("./question");
 const user = require("./user");
+const admin = require("./admin");
 
 const { getUserSessionInfo } = require("../middlewares/authorization/auth");
 
@@ -9,6 +10,7 @@ router.use(getUserSessionInfo);
 router.use("/questions", questions);
 router.use("/auth", auth);
 router.use("/users", user);
+router.use("/admin", admin);
 
 router.get("/", (req, res, next) => {
     res.render("index");
