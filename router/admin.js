3e3988c76ef0b0eb52c20b0aeae593a65da17c3f
Ferhat <fkmll2121@gmail.com>
2023-01-28 23:34:08 +0300

    Set the user blocking router
    The admin routeris set to block or unblock users.

--- router/admin.js
+++ router/admin.js
@@ -3,16 +3,15 @@ const {
     getAccessToRoute,
     getAdminAccess,
 } = require("../middlewares/authorization/auth");
+const { blockUser } = require("../controller/adminController");
+const {
+    checkUserExist,
+} = require("../middlewares/database/databaseErrorHelpers");
 
 const router = express.Router();
 
 router.use([getAccessToRoute, getAdminAccess]);
 
-router.get("/", (req, res, next) => {
-    res.status(200).json({
-        success: true,
-        message: "Admin Page",
-    });
-});
+router.get("/block/:id", checkUserExist, blockUser);
 
 module.exports = router;
